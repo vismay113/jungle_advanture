8874d0132b8ab2ee5e34e0acf64f45166c858fb2
vismaysmac <vismay.patel1103@gmail.com>
2019-08-30 07:31:48 +0530

    assets
    assets are added

---
+++ Assets/GameStates.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "GameStates")]
+public class GameStates : ScriptableObject 
+{
+    [TextArea(14, 10)] [SerializeField] string storyText;
+    [SerializeField] GameStates[] comingStates;
+
+    public string GetStateStory ()
+    {
+        return storyText;
+    }
+
+    public GameStates[] GetNextStates ()
+    {
+        return comingStates;
+    }
+}
---
+++ Assets/advantureGame.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class advantureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] Text errorComponent;
+    [SerializeField] GameStates state1;
+
+    GameStates currentState;
+
+    //string StoryText = "Ramesh is a father of son and daughter named Vismay and Ankita. They were going to their river house for the vacation. But on the way, a pack of tigers attacked them and they have to fight their way to home. While fighting with tigers Rameh is died and 2 sibilings split away. Your objective is to find your other sibling and make your way to river home.";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = state1;
+        textComponent.text = currentState.GetStateStory();
+        errorComponent.text = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = currentState.GetNextStates();
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentState = nextStates[0];
+            errorComponent.text = null;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentState = nextStates[1];
+            errorComponent.text = null;
+        }
+        else if (Input.anyKeyDown)
+        {
+            errorComponent.text = ("PLEASE SELECT A VALID OPTION !");
+            Debug.Log("from else part");
+        }
+
+        textComponent.text = currentState.GetStateStory();
+    }
+}
